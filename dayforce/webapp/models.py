3cb7bd018f3835955efbccfd1a92adf1ef3933ab
yamini236 <74612040+yamini236@users.noreply.github.com>
2021-08-28 01:51:44 +0530

    modify register

--- dayforce/webapp/models.py
+++ dayforce/webapp/models.py
@@ -4,6 +4,9 @@ class Register(models.Model):
 	email=models.CharField(max_length=50)
 	password=models.CharField(max_length=50)
 	contact=models.CharField(max_length=50)
+    address=models.CharField(max_length=50)
+    gender=models.ChoiceField
+    
 
 	def __str__(self):
 		return self.name
